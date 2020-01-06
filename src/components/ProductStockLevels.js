ca1a9b662ef74a76721902114bee45c014820198
Nelson Wu <nelson@nelsonwu.net>
2020-01-06 19:49:24 +1100

    Reorganised, and added header to stock results

--- src/components/ProductStockLevels.js
+++ src/components/ProductStockLevels.js
@@ -1,23 +1,28 @@
 import React from 'react';
-import ProductStockLevel from './ProductStockLevel';
 
 function ProductStockLevels({stock}) {
     return (
         <div>
-        {
-            stock.map((stockLevel, index) => 
+            <div className="cf bb">
+                <div className="fl w-20 pa1">State</div>
+                <div className="fl w-20 pa1">Postcode</div>
+                <div className="fl w-20 pa1">Suburb</div>
+                <div className="fl w-20 pa1">Stock Status</div>
+                <div className="fl w-20 pa1">Stock Count</div>
+            </div>
             {
-                return (
-                    <ProductStockLevel 
-                        key={index} 
-                        storeState={stockLevel.StoreInfo.State} 
-                        storePostcode={stockLevel.StoreInfo.Postcode} 
-                        storeSuburb={stockLevel.StoreInfo.Suburb} 
-                        stockMessage={stockLevel.StockStatus[0].Message} 
-                        stockCount={stockLevel.StockStatus[0].StockCount} 
-                    />);
-            }) 
-        }
+                stock.map((stockLevel, index) => 
+                {
+                    return (
+                        <div className="cf bt bb">
+                            <div className="fl w-20 pa1">{stockLevel.StoreInfo.State}</div>
+                            <div className="fl w-20 pa1">{stockLevel.StoreInfo.Postcode}</div>
+                            <div className="fl w-20 pa1">{stockLevel.StoreInfo.Suburb}</div>
+                            <div className="fl w-20 pa1">{stockLevel.StockStatus[0].Message}</div>
+                            <div className="fl w-20 pa1">{stockLevel.StockStatus[0].StockCount}</div>
+                        </div>);
+                }) 
+            }
         </div>
     );
 }
